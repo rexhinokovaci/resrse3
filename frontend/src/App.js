c5e5bc150c3330138669a7a429f2b38b7bd6241f
Rexhino Kovaci <kovacirexhino@gmail.com>
2021-11-23 12:18:21 +0100

    Second

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,25 +5,28 @@ import { Button } from 'react-bootstrap'
 import makeschool from "./makeschool.png"
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 const App = () => {
   return (
+
+    
     <div>
-      <h1>This is how the lead generation would work</h1>
-      <img src={logo} className="testing" alt="Logo" />
-      <br></br>
-      <hr></hr>
-      <h2>Someone who is winning money photo</h2>
-      <img src={makeschool} alt="TEST image" />
-      <hr></hr>
-      <h2>Test Bootstrap Buttons</h2>
-      <Button bsStyle="primary">First name</Button>{' '}
-      <Button type="button" class="btn btn-primary">Last name</Button>{' '}
-      <Button variant="success">Send Data</Button>{' '}
-      <Button variant="warning">Send Data</Button>{' '}
-
-      <button type="button" class="btn btn-primary">Primary BootStrap</button>
-<button type="button" class="btn btn-secondary">Secondary Bootstap</button>
-
+      <form action="https://formspree.io/f/xzbyqloz" method="POST">
+  <label>
+    <input type="email" name="_replyto"/>
+  </label>
+  <label>
+    <textarea name="message"></textarea>
+    <textarea name="message"></textarea>
+    <textarea name="message"></textarea>
+    <textarea name="message"></textarea>
+    <textarea name="message"></textarea>
+    <textarea name="message"></textarea>
+  </label>
+  
+  <button type="submit">Send</button>
+</form>
+      
     </div>
 
   )
